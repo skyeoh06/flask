5bc11a23ddd537a3fb3296975e22a2d1c30e18fd
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2025-03-13 15:48:54 +0800

    Create app.py

---
+++ 03-hotel-room-booking/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+room_types = {
+    "single-bed": "Single Bed",
+    "double-bed": "Double Bed",
+    "deluxe": "Deluxe Suite"
+}
+
+
+@app.route('/')
+def home():
+    return render_template('show_form.template.html')
+
+
+@app.route('/', methods=['POST'])
+def process_form():
+    print(request.form)
+    first_name = request.form.get('first-name')
+    last_name = request.form.get('last-name')
+    comments = request.form.get('comments')
+    room_type = request.form.get('room')
+
+    # for checkboxes, we have to use form.request.getlist()
+    amenities = request.form.getlist('amenities')
+
+    check_in_timing = request.form.get('check-in-timing')
+
+    return render_template('results.template.html',
+                           fname=first_name,
+                           lname=last_name,
+                           comments=comments,
+                           room_type=room_types[room_type],
+                           amenities=", ".join(amenities),
+                           check_in_timing=check_in_timing)
+
+
+@app.route('/survey')
+def show_survey():
+    return render_template("survey.template.html")
+
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about_us.template.html')
+
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
